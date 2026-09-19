aca93a9a6dc5edd6d84a16a9b67481e327cff5a4
eq <estuardo.quan@gmail.com>
2026-09-19 16:48:15 -0600

    changes

--- dist/alpine/configs/custom.sh
+++ dist/alpine/configs/custom.sh
@@ -8,8 +8,12 @@ sed -i '/^#.*\/community$/s/^#//' /etc/apk/repositories
 apk update
 apk add --no-cache \
     cloud-init cloud-init-openrc \
+    mount \
     docker \
     curl git tmux e2fsprogs-extra
+
+# replace mdev with eudev: provides /dev/virtio-ports/* for qemu-guest-agent
+setup-devd udev
 
 rc-update add cloud-init-local boot
 rc-update add cloud-init default
